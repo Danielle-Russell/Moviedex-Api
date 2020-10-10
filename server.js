ee062c1ac18c2437491cd51735501e2f2e56910a
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2020-10-10 15:57:57 -0700

    production

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const MOVIES = require('./movie-data.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+ app.use(morgan(morganSetting))
 
 app.use(function validateBearerToken(req, res, next) {
      const apiToken = process.env.API_TOKEN
      const authToken = req.get('Authorization')
-      console.log('validate bearer token middleware')
       if (!authToken || authToken.split(' ')[1] !== apiToken) {
             return res.status(401).json({ error: 'Unauthorized request' })
           }
@@ -51,8 +51,17 @@ res.json(result)
 
 app.get('/movie', getMovies)
 
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+  
 
-const port = 7000;
+const PORT = process.env.PORT || 8000
 app.listen(port, () => {
-    console.log(`Server listening at port ${port}`)
 } )
